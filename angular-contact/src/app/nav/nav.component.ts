5e0328f093072f900c3de88de67ef30f6d31788c
Lara Correa <lara.mcj@gmail.com>
2022-01-09 18:17:28 -0300

    formulário ajustado

--- angular-contact/src/app/nav/nav.component.ts
+++ angular-contact/src/app/nav/nav.component.ts
@@ -3,32 +3,31 @@ import { Component, HostListener, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 
 export class NavComponent implements OnInit {
-
+  
   responsivity = true;
   toggle = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-    if (window.innerWidth <= 960) {
-      this.responsivity = false
-    }
-
-  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
     event.target.innerWidth;
-    if (window.screen.width <= 960) { 
+    console.log(event.target.innerWidth);
+    if (window.screen.width <= 960) {
       this.responsivity = false;
-      console.log('atingi o tamanho')
-    } else{
+      console.log('atingi o tamanho');
+    } else {
       this.responsivity = true;
     }
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    if (window.innerWidth <= 960) {
+      this.responsivity = false;
+    }
+  }
 }
